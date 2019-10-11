07483ed8a013ed4dbdf4dcdae598a70a4d615e21
Ashwin R C <ashwin2001achu@gmail.com>
2019-10-11 14:12:24 +0000

    op7t: init from op7

--- lineage_hotdogb.mk
+++ lineage_hotdogb.mk
@@ -8,17 +8,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from guacamoleb device
-$(call inherit-product, device/oneplus/guacamoleb/device.mk)
+# Inherit from hotdogb device
+$(call inherit-product, device/oneplus/hotdogb/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_guacamoleb
-PRODUCT_DEVICE := guacamoleb
+PRODUCT_NAME := lineage_hotdogb
+PRODUCT_DEVICE := hotdogb
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := OnePlus 7
+PRODUCT_MODEL := OnePlus 7T
 PRODUCT_MANUFACTURER := OnePlus
 
 PRODUCT_AAPT_CONFIG := xxxhdpi
@@ -30,10 +30,10 @@ TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
 # Build info
-BUILD_FINGERPRINT := "OnePlus/OnePlus7/OnePlus7:9/PKQ1.190110.001/1907280700:user/release-keys"
+BUILD_FINGERPRINT := "OnePlus/OnePlus7T/OnePlus7T:10/QKQ1.190716.003/1909250940:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=OnePlus7 \
     PRODUCT_NAME=OnePlus7 \
-    PRIVATE_BUILD_DESC="OnePlus7-user 9 PKQ1.190110.001 1907280700 release-keys"
+    PRIVATE_BUILD_DESC="OnePlus7T-user 10 QKQ1.190716.003 1909250940 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
